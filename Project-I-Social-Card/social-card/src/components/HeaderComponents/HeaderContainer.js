eaf9b309d3b86dc93e75674d13eaba6facc003a9
joerlop <jj.erlich155@gmail.com>
2019-04-22 14:35:31 -0500

    Finished ImageThumbnail component and HeaderTitle component

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import './Header.css';
+
+import "./ImageThumbnail";
+
+function HeaderContainer() {
+    return (
+        <div className="header-container">
+            <ImageThumbnail />    
+        </div>                
+    );
+}
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,15 @@
 import React from 'react';
 import './Header.css';
+
+let currentDate = new Date();
+
+function HeaderTitle() {
+    return (
+        <div className="header-title">
+            <h2>Lambda School</h2>
+            <p>@LambdaSchool | ${currentDate.getDate()}</p>
+        </div>                
+    );
+}
+
+export default HeaderTitle;
