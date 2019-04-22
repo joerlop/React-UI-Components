4550e62bca7160f6ab1af328fb3375998dd7529b
joerlop <jj.erlich155@gmail.com>
2019-04-22 17:29:06 -0500

    Final commit - styled social media icons

--- Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
+++ Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -7,7 +7,7 @@ function Footer() {
     return (
         <div className="footer-container">
             <i className="far fa-comment"></i>
-            <p id="comment" className="smComment no-show">{commentCount}</p>
+            <p id="comment" className="smComment">{commentCount}</p>
             <i className="fas fa-sync"></i>
             <p className="count no-show"></p>
             <i className="far fa-heart"></i>
@@ -18,12 +18,18 @@ function Footer() {
     );
 }
 
-const comment = document.getElementsByClassName("smComment");
+function count() {
+    return commentCount++;
+}
+
+/*
+const comment = document.querySelector(".smComment");
 console.log(comment);
 
 comment.addEventListener("click", target => {
     commentCount++;
     comment.classList.remove("no-show");
 })
+*/
 
 export default Footer;
