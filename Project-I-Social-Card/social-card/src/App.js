d54e0f2475df7831d59d6bc0e11f94e4c462d613
joerlop <jj.erlich155@gmail.com>
2019-04-22 16:20:04 -0500

    Completed and styled card component

--- Project-I-Social-Card/social-card/src/App.js
+++ Project-I-Social-Card/social-card/src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 
 import "./components/HeaderComponents/HeaderContainer";
 import HeaderContainer from './components/HeaderComponents/HeaderContainer';
+import CardContainer from './components/CardComponents/CardContainer';
 
 const App = () => {
   return (
     <div className="app">
       <HeaderContainer />
+      <CardContainer />
     </div>
   );
 };
--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -1,2 +1,18 @@
 import React from 'react';
 import './Card.css';
+
+import CardBanner from './CardBanner';
+import CardContent from "./CardContent";
+
+function CardContainer() {
+    return (
+        <a className="card-container" target="_blank" href="https://www.reactjs.org">
+            <div>
+                <CardBanner />
+                <CardContent />    
+            </div>
+        </a>                
+    );
+}
+
+export default CardContainer;
