b21f99d4f09539ee3bc77f4360fe59fbc7d2a0d9
joerlop <jj.erlich155@gmail.com>
2019-04-23 16:59:21 -0500

    Stretch: added state object with total property

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -5,13 +5,20 @@ import CalculatorDisplay from "./components/DisplayComponents/CalculatorDisplay"
 import ButtonContainer from "./components/ButtonComponents/ButtonContainer";
 
 class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {total: 0}
+  }
+
   render() {
     return (
       <div className="main-container">
         <CalculatorDisplay />
         <ButtonContainer />
       </div>)
-  }
+  };
 }
 
 export default App;
